3de720f9773322ab2798c9aa627b0fd42b100da2
wolfcode <autofit@163.com>
2021-10-23 02:05:13 +0800

    Update autofit.go

--- autofit/autofit.go
+++ autofit/autofit.go
@@ -9,24 +9,22 @@ import (
 )
 
 var (
-	i int64 = 0
-	x int   = 0
-	//Second int   = 0
-	lock   sync.RWMutex
-	err    error
-	t      = time.Now()
-	Second = t.Second()
+	i           int64 = 0
+	x           int   = 0
+	SecondGetId int   = 0
+	lock        sync.RWMutex
+	err         error
 )
 
 func TcpId(addr string) {
 	t := time.Now()
-	Second = t.Second()
+	Second := t.Second()
 	yearMap := make(map[int64]string)
 	dateMap := make(map[int64]string)
-	baseTable := []byte("123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbn")
-	yearTable := make([]int64, 60)
+	baseTable := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+	yearTable := make([]int64, 62)
 	n := int64(2021)
-	for i := 0; i < 60; i++ {
+	for i := 0; i < 62; i++ {
 		n = int64(2021 + i)
 		yearTable[i] = n
 	}
@@ -52,14 +50,12 @@ func TcpId(addr string) {
 			defer c.Close()
 
 			for {
-
 				d := make([]byte, 1024)
 				_, err := c.Read(d)
 				if err != nil {
 					log.Printf("Error reading TCP session: %s", err)
 					break
 				}
-				//log.Printf("reading data from client: %s\n", string(d))
 
 				t := time.Now()
 				if t.Second() > 58 {
@@ -98,12 +94,14 @@ func TcpId(addr string) {
 	}
 }
 func GetId() string {
+	t := time.Now()
+	SecondGetId = t.Second()
 	yearMap := make(map[int64]string)
 	dateMap := make(map[int64]string)
-	baseTable := []byte("123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbn")
-	yearTable := make([]int64, 60)
+	baseTable := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+	yearTable := make([]int64, 62)
 	n := int64(2021)
-	for i := 0; i < 60; i++ {
+	for i := 0; i < 62; i++ {
 		n = int64(2021 + i)
 		yearTable[i] = n
 	}
@@ -111,18 +109,18 @@ func GetId() string {
 		dateMap[int64(k)] = string(v)
 		yearMap[int64(yearTable[k])] = string(v)
 	}
-	t = time.Now()
+
 	if t.Second() > 58 {
 		if x == 0 {
-			Second = 0
+			SecondGetId = 0
 		}
 		x++
 	}
 	if t.Second() < 1 {
 		x = 0
 	}
-	if t.Second() > Second {
-		Second = t.Second()
+	if t.Second() > SecondGetId {
+		SecondGetId = t.Second()
 		i = 0
 	}
 	aaa := yearMap[int64(t.Year())] + dateMap[int64(t.Month())] + dateMap[int64(t.Day())] + dateMap[int64(t.Hour())] + dateMap[int64(t.Minute())] + dateMap[int64(t.Second())] + fmt.Sprint(i)
