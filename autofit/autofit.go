b454dc2c12e2d38cc7e74538e1cdf7fb3d9d003b
wolfcode <autofit@163.com>
2021-11-01 20:55:55 +0800

    Update autofit.go

--- autofit/autofit.go
+++ autofit/autofit.go
@@ -151,6 +151,9 @@ func GetId() string {
 	}
 	aaa := yearMap[int64(t.Year())] + dateMap[int64(t.Month())] + dateMap[int64(t.Day())] + dateMap[int64(t.Hour())] + dateMap[int64(t.Minute())] + dateMap[int64(t.Second())] + Bit62Adder(i)
 	lock.Lock()
+	if t.Second() < 1 {
+		x = 0
+	}
 	i++
 	lock.Unlock()
 	return aaa
